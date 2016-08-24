288040bccdd9bc7a059b9f0cf55666ec09ada4b1
Franck Nouama <franck.nouama@gmail.com>
2016-08-23 22:43:32 -0400

    Reverse program

--- ch4/rev/main.go
+++ ch4/rev/main.go
@@ -9,11 +9,8 @@ func reverse(s []int) {
 }
 
 func main() {
-	println("Demo reverse with Go Slice")
 	a := [...]int{0, 1, 2, 3, 4, 5}
-	println("Before:")
 	fmt.Println(a)
 	reverse(a[:])
-	println("After:")
 	fmt.Println(a)
 }
